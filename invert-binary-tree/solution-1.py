cc194d10b5eb930ebf8cf9a42f5705df4ebf5ade
The-Flash <absaugustineflash@gmail.com>
2023-09-25 16:33:10 +0000

    invert binary tree iterative solution

--- invert-binary-tree/solution-1.py
+++ invert-binary-tree/solution-1.py
@@ -1,6 +1,15 @@
 def invertBinaryTree(tree):
-    pass
+    queue =[tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftAndRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
 
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
 
 class BinaryTree:
     def __init__(self, value):
